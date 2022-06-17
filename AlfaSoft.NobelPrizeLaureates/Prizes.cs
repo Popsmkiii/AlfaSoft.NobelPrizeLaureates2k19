0fcbd225ea5ce9004145eaaacd1ba3116454fd44
Alexandre Alves <dragonthereal1@gmail.com>
2022-06-17 11:37:36 +0100

    First step creations; Class Prizes to store objects;

---
+++ AlfaSoft.NobelPrizeLaureates/Prizes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlfaSoft.NobelPrizeLaureates
+{
+    class Prizes
+    {
+        public string Name { get; set; }
+        public DateTime Year { get; set; }
+
+        public Prizes(string name, DateTime year)
+        {
+            Name = name;
+            Year = year;
+        }
+    }
+}
--- AlfaSoft.NobelPrizeLaureates/Program.cs
+++ AlfaSoft.NobelPrizeLaureates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace AlfaSoft.NobelPrizeLaureates
 {
@@ -6,7 +7,47 @@ namespace AlfaSoft.NobelPrizeLaureates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string directoryStr = "";
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Directory (full path) to a file:\n");
+                directoryStr = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(directoryStr))
+                {
+                    do
+                    {
+                        Console.WriteLine("Please, insert a directory (full path. Ex.: C:\\Users\\Administrator\\Desktop\\File Ex\\Example.txt) to a file:\n");
+                        directoryStr = Console.ReadLine();
+                    } while (string.IsNullOrEmpty(directoryStr));
+                }
+
+                Console.WriteLine("Checking if file exists or is valid...");
+
+                if (File.Exists(directoryStr))
+                {
+                    Console.WriteLine("Validation complete! File exists");
+                }
+                else
+                {
+                    Console.WriteLine("Validation failed. Make sure your file is of valid type (.txt)");
+                }
+            }
+            else
+            {
+                foreach (var item in args)
+                {
+                    directoryStr += item.ToString();
+                }
+                Console.WriteLine("Current directory is: " + directoryStr);
+                Console.WriteLine("Is this correct? (y/n)");
+                string userInput = Console.ReadLine();
+                
+                if(userInput.ToLower() == "y" || userInput.ToLower() == "yes")
+                {
+
+                }
+            }
         }
     }
 }
