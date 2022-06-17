ad51d2ca65996e772acdb6bb21bc513718a2a4af
Administrator <Administrator@ALEXALVES>
2022-06-17 11:21:12 +0100

    Add project files.

---
+++ AlfaSoft.NobelPrizeLaureates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlfaSoft.NobelPrizeLaureates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
